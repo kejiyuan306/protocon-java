77939ffe3fc8c50249e349abb13e2eed9d516bb4
Bomou He <paakmau@outlook.com>
2022-02-20 01:02:49 +0800

    Use Setter for ConfiguationProperties

--- src/main/java/com/example/protocon/config/ProtoconAutoConfiguration.java
+++ src/main/java/com/example/protocon/config/ProtoconAutoConfiguration.java
@@ -6,9 +6,9 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import lombok.Data;
+import lombok.Setter;
 
-@Data
+@Setter
 @Configuration
 @ConfigurationProperties(prefix = "protocon")
 public class ProtoconAutoConfiguration {
